9127aa91337b36dc24ff46bcefd68b49d308de65
wxx9248 <wxx9248@qq.com>
2019-12-29 03:45:05 -0500

    2019-12-29 Uploaded

---
+++ DDNSMon.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+         Cloudflare DDNS system v2.0
+                 by wxx9248
+
+    Licensed by GNU General Public License v3.0
+           Copyright 2020 © wxx9248
+"""
+
+__author__      = "wxx9248"
+__copyright__   = "Copyright 2020 © wxx9248"
+__license__     = "GPL v3"
+__version__     = "v2.0"
+__maintainer__  = [__author__]
+__credits__     = [__author__]
+__email__       = "wxx9248@qq.com"
+__status__      = "Development"
