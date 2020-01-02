220a271bba1138ecd8ec3050825232bcfdaec913
wxx9248 <wxx9248@qq.com>
2020-01-02 16:55:10 -0500

    2020-01-02 Uploaded

--- DDNSMon.py
+++ DDNSMon.py
@@ -3,11 +3,11 @@
 
 """
 ------------------------------------------------------
-           Cloudflare DDNS system v2.0
-                   by wxx9248
+            Cloudflare DDNS system v2.0
+                    by wxx9248
 
     Licensed under GNU General Public License v3.0
-             Copyright 2020 © wxx9248
+              Copyright 2020 © wxx9248
 ------------------------------------------------------
 """
 
@@ -21,19 +21,13 @@ __email__       = "wxx9248@qq.com"
 __status__      = "Development"
 
 import os, sys
+import json
 import logging
 import ctypes
+import re
 
-def clrscr():
-    dllname = "clrscr.dll"
-    logger = logging.getLogger(__name__)
-    
-    try:
-        dll = ctypes.CDLL(dllname)
-    except Exception as e:
-        logger.error("Can't load " + dllname + ": " + str(e))
-    else:
-        dll.clrscr()
+CONFPATH     = "conf.json"
+UNKNOWNEXMSG = "Unknown exception occurred, referring to information below."
 
 
 def main():
@@ -42,19 +36,73 @@ def main():
         "E-mail":                 "",
         "Zone-ID":                "",
         "GlobalAPIMode":          False,
-        "IPv6":                   True,
+        "IPv6":                   False,
         "DNSAPIToken":            "",
         "GlobalAPIKey":           "",
         "Encrypted":              False
     }
-    logging.basicConfig(level = logging.INFO, format = "[%(asctime)s] %(name)s: %(funcName)s(): [%(levelname)s] %(message)s")
     logger = logging.getLogger(__name__)
 
     # Start
     clrscr()
     print(sys.modules[__name__].__doc__)
     
+    try:
+        conffile = open(CONFPATH)
+    except FileNotFoundError:
+        # First run
+        logger.info("Configure file not found.")
+        logger.info("Entering first-run configuration...")
+        firstrun()
+    except OSError as e:
+        logger.error("Can't open configure file \"{}\" for reading".format(CONFPATH))
+        raise
+    except Exception as e:
+        logger.error(UNKNOWNEXMSG)
+        raise
+
+
+def clrscr():
+    dllname = "clrscr.dll"
+    logger = logging.getLogger(__name__)
+    logger.debug("Logger initialized.")
+    
+    try:
+        dll = ctypes.CDLL(dllname)
+        logger.debug("DLL attached.")
+    except OSError as e:
+        logger.error("Can't load " + dllname)
+    except Exception:
+        logger.error(UNKNOWNEXMSG)
+        raise
+    else:
+        dll.clrscr()
+
+def firstrun():
+    logger = logging.getLogger(__name__)
+    logger.debug("Logger initialized.")
+
+    try:
+        conffile = open(CONFPATH, "w")
+    except OSError as e:
+        logger.error("Can't open configure file \"{}\" for writing".format(CONFPATH))
+        raise
+    except Exception:
+        logger.error(UNKNOWNEXMSG)
+        raise
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(level = logging.INFO, format = "[%(asctime)s] %(name)s: %(funcName)s(): [%(levelname)s] %(message)s")
+    logger = logging.getLogger(__name__)
+    logger.debug("Logger initialized.")
+
+    try:
+        main()
+    except Exception as e:
+        logger.error("*********************************")
+        logger.error("A fatal error has occurred below:\n")
+        logger.error(re.search(r"<class '(.+)'>", str(e.__class__)).group(1) + ": " + str(e) + "\n")
+        logger.error("Program exits abnormally.")
+        logger.error("*********************************")
+
